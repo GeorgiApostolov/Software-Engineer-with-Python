b21b46230bc10272509efb2c7f6af1d4c008e3d8
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2026-07-04 21:51:17 +0300

    Create 09_yard_greening.py

---
+++ Programming Basics with Python - July 2026/01_first_steps_in_programming/lab/09_yard_greening.py
@@ -0,0 +1,5 @@
+yards = float(input())
+total_price = yards*7.61
+discount = total_price * 0.18
+print(f"The final price is: {total_price - discount} lv.")
+print(f"The discount is: {discount} lv.")

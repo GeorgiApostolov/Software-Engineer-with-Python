ca447efd0576e316228c8d3d07e502140be7e2b6
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2026-07-04 21:51:15 +0300

    Create 08_pet_shop.py

---
+++ Programming Basics with Python - July 2026/01_first_steps_in_programming/lab/08_pet_shop.py
@@ -0,0 +1,7 @@
+dog_food = float(input())
+cat_food = float(input())
+
+dog_total_price = float(dog_food*2.50)
+cat_total_price = float(cat_food*4)
+total_price = cat_total_price + dog_total_price
+print(f"{total_price} lv.")

0a4d4d5f0cbc7860cae89399a28568dcd9abace4
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2026-07-04 21:51:13 +0300

    Create 07_projects_creation.py

---
+++ Programming Basics with Python - July 2026/01_first_steps_in_programming/lab/07_projects_creation.py
@@ -0,0 +1,4 @@
+architect_name = input()
+project_count = int(input())
+total_hours = int(project_count*3)
+print(f"The architect {architect_name} will need {total_hours} hours to complete {project_count} project/s.")

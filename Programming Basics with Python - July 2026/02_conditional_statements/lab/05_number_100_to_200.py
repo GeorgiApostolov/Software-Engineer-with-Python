60d2d9535a8d3999e620187c8a26d4e81c9ba074
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2026-08-18 19:50:47 +0300

    Create 05_number_100_to_200.py

---
+++ Programming Basics with Python - July 2026/02_conditional_statements/lab/05_number_100_to_200.py
@@ -0,0 +1,8 @@
+input_num = int(input())
+
+if input_num < 100:
+    print("Less than 100")
+elif (input_num >= 100) and (input_num <= 200):
+    print("Between 100 and 200")
+else:
+    print("Greater than 200")

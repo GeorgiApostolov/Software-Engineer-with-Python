45c690167c6b86727061ddf03a815662a9229c75
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2026-09-01 20:07:26 +0300

    Create 01_day_of_week.py

---
+++ Programming Basics with Python - July 2026/03_conditional_statements_advanced/lab/01_day_of_week.py
@@ -0,0 +1,18 @@
+input_day = int(input())
+
+if input_day == 1:
+    print("Monday")
+elif input_day == 2:
+    print("Tuesday")
+elif input_day == 3:
+    print("Wednesday")
+elif input_day == 4:
+    print("Thursday")
+elif input_day == 5:
+    print("Friday")
+elif input_day == 6:
+    print("Saturday")
+elif input_day == 7:
+    print("Sunday")
+else:
+    print("Error")

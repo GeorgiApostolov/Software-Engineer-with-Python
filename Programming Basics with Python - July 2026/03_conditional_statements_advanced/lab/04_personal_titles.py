53ce379fbcbc1a351cd8a2c298a15998247b5a80
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2026-09-11 18:11:53 +0300

    Create 04_personal_titles.py

---
+++ Programming Basics with Python - July 2026/03_conditional_statements_advanced/lab/04_personal_titles.py
@@ -0,0 +1,17 @@
+#•	"Mr." – мъж (пол 'm') на 16 или повече години
+#•	"Master" – момче (пол 'm') под 16 години
+#•	"Ms." – жена (пол 'f') на 16 или повече години
+#•	"Miss" – момиче (пол 'f') под 16 години
+
+
+age = float(input())
+sex = input()
+
+if sex == 'm' and age < 16:
+    print("Master")
+elif sex == 'm' and age >= 16:
+    print("Mr.")
+elif sex =="f" and age < 16:
+    print("Miss")
+else:
+    print("Ms.")
